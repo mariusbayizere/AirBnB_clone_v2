c0711fc696f736eced24de50913abe5e288439df
bayizeremarius119@gmail.com <bayizeremarius119@gmail.com>
2024-03-22 12:00:00 +0200

    modified-file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from fabric.api import local
 from os import path
 
+
 def do_pack():
     """
     Generate a tgz archive from the web_static folder contents.
@@ -14,6 +15,7 @@ def do_pack():
         The archive path if the archive has been correctly generated.
         None otherwise.
     """
+
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         file_name = "versions/web_static_{}.tgz".format(date)
@@ -21,7 +23,8 @@ def do_pack():
             local("mkdir versions")
         local("tar -cvzf {} web_static".format(file_name))
         archived_size = local("wc -c < {}".format(file_name), capture=True)
-        print("web_static packed: {} -> {}Bytes".format(file_name, archived_size))
+        print("web_static packed: {} -> {}Bytes".format(
+            file_name, archived_size))
         return file_name
     except Exception:
         return None
