455e406162ff41e7abe976d377711f0e5c840a22
bayizeremarius119@gmail.com <bayizeremarius119@gmail.com>
2024-03-22 12:00:00 +0200

    10.DBStorage-Amenity...andBOOM!

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""
+Module for Amenity class.
+"""
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """
+    Amenity class that inherits from BaseModel and Base.
+    Represents amenities in the database.
+    """
+    __tablename__ = 'amenities'
+
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity")
